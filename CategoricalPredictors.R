05607d33a6f6a26ab2781084be4a299583851b4a
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2018-06-29 12:48:54 +0100

    Shown importance of factor level order on lm(er) analysis

--- CategoricalPredictors.R
+++ CategoricalPredictors.R
@@ -15,8 +15,8 @@ df <- tibble(Participant = parse_factor(rep(1:100, 2), levels = NULL),
                0      * (Intervention == "Mindfulness Meditation") +
                (-2)   * (PrePost == "PostIntervention") +
                (-100) * (Intervention == "Mindfulness Meditation")*(PrePost == "PostIntervention") +
-               rnorm(200, 0, 25) + # General variance
-               rep(rnorm(100, 0, 5), 2)
+               rnorm(200, 0, 25) +      # General variance
+               rep(rnorm(100, 0, 5), 2) # Participant-specific variance
 )
 
 # DATA DESCRIPTION =================================================================================
@@ -36,6 +36,7 @@ bplot <- ggplot(data = df,
 stress.anova <- aov(StressLevel ~ Intervention*PrePost,
                     data = df)
 (summary(stress.anova))
+
 # Regular lm: same computations as ANOVA but different way of showing results
 ## From the aov function help:
 ### The main difference from lm is in the way print, summary and so on handle the fit:
@@ -48,4 +49,15 @@ stress.lm <- lm(StressLevel ~ Intervention*PrePost,
 # Mixed-effect model, just to compare output to ANOVA output and lm output
 stress.lmer <- lmer(StressLevel ~ Intervention*PrePost + (1 | Participant),
                     data = df)
-(summary(stress.lm))
+(summary(stress.lmer))
+
+# Re-run aov and lm with different factor order for Intervention
+df2 <- df
+df2$Intervention <- relevel(df2$Intervention, ref = "Mindfulness Meditation") # New ref level
+stress.anova2 <- aov(StressLevel ~ Intervention*PrePost,
+                     data = df)
+(summary(stress.anova2)) # No difference
+stress.lm2 <- lm(StressLevel ~ Intervention*PrePost,
+                data = df2)
+(summary(stress.lm2))    # Now main effect of PrePost because this is for participants in the
+                         # Mindfulness Meditation group only
