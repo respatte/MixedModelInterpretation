c0b42d99ac9c767d995e13f2f258ba0d856e852d
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2018-07-13 21:20:12 +0100

    lancaster conf included results for categorical ANOVA and lm

--- src/CategoricalPredictors.R
+++ src/CategoricalPredictors.R
@@ -4,32 +4,32 @@ library(tidyverse)
 library(RColorBrewer)
 source("geom_flat_violin.R")
 
-# DATA GENERATION ==================================================================================
-df <- tibble(Participant = parse_factor(rep(1:100, 2), levels = NULL),
-             Intervention = parse_factor(rep(c("Video Game", "Mindfulness Meditation"), 100),
-                                         levels = c("Video Game",                # Reference level
-                                                    "Mindfulness Meditation")),  # Comparison level
-             PrePost = parse_factor(c(rep("Pre Intervention", 100),
-                                      rep("Post Intervention", 100)),
-                                    levels = c("Pre Intervention",                # Reference level
-                                               "Post Intervention")),             # Comparison level
-             StressLevel = 150 +
-               0      * (Intervention == "Mindfulness Meditation") +
-               (-2)   * (PrePost == "Post Intervention") +
-               (-100) * (Intervention=="Mindfulness Meditation")*(PrePost=="Post Intervention") +
-               rnorm(200, 0, 25) +      # General variance
-               rep(rnorm(100, 0, 5), 2) # Participant-specific variance
+# CATEGORICAL DATA GENERATION ======================================================================
+cat <- tibble(Participant = parse_factor(rep(1:100, 2), levels = NULL),
+              Intervention = parse_factor(rep(c("Video Game", "Mindfulness Meditation"), 100),
+                                          levels = c("Video Game",                # Reference level
+                                                     "Mindfulness Meditation")),  # Comparison level
+              PrePost = parse_factor(c(rep("Pre Intervention", 100),
+                                       rep("Post Intervention", 100)),
+                                     levels = c("Pre Intervention",               # Reference level
+                                                "Post Intervention")),            # Comparison level
+              StressLevel = 150 +
+                0      * (Intervention == "Mindfulness Meditation") +
+                (-2)   * (PrePost == "Post Intervention") +
+                (-100) * (Intervention=="Mindfulness Meditation")*(PrePost=="Post Intervention") +
+                rnorm(200, 0, 25) +      # General variance
+                rep(rnorm(100, 0, 5), 2) # Participant-specific variance
 )
 # The experiment is as follow: we measure stress levels in participants before and after an
 # intervention. The intervnetion consists of either a session of mindfulness meditation,
 # or a session of playing video games.
 
-# DATA DESCRIPTION =================================================================================
+# CATEGORICAL DATA DESCRIPTION =====================================================================
 # Plot
-barplot <- ggplot(data = df,
-                  aes(y = StressLevel,
-                      x = PrePost,
-                      fill = Intervention)) +
+cat.barplot <- ggplot(data = cat,
+                      aes(y = StressLevel,
+                          x = PrePost,
+                          fill = Intervention)) +
   geom_bar(stat = "summary", fun.y = "mean",
            position = "dodge",
            width = .5) +
@@ -42,14 +42,14 @@ barplot <- ggplot(data = df,
         legend.key.size = unit(.5, "lines"),
         legend.box.spacing = unit(0, "mm"),
         text = element_text(size = 8))
-ggsave("CategoricalBarplot.pdf", barplot,
+ggsave("CategoricalBarplot.pdf", cat.barplot,
        width = 3, height = 2, dpi = 600)
 
-raincloud <- ggplot(data = df,
-                    aes(x = Intervention,
-                        y = StressLevel,
-                        fill = Intervention,
-                        colour = Intervention)) +
+cat.raincloud <- ggplot(data = df,
+                        aes(x = Intervention,
+                            y = StressLevel,
+                            fill = Intervention,
+                            colour = Intervention)) +
   geom_flat_violin(position = position_nudge(x = .2, y = 0),
                    alpha = .7, width = .7,
                    colour = NA) +
@@ -68,23 +68,23 @@ raincloud <- ggplot(data = df,
         legend.key.size = unit(.5, "lines"),
         legend.box.spacing = unit(0, "mm"),
         text = element_text(size = 8))
-ggsave("CategoricalRaincloud.pdf", raincloud,
+ggsave("CategoricalRaincloud.pdf", cat.raincloud,
        width = 4, height = 2, dpi = 600)
 
-# DATA ANALYSIS ====================================================================================
+# CATEGORICAL DATA ANALYSIS ========================================================================
 # Regular ANOVA
-stress.anova <- aov(StressLevel ~ Intervention*PrePost,
-                    data = df)
-(summary(stress.anova))
+cat.anova <- aov(StressLevel ~ Intervention*PrePost,
+                    data = cat)
+(summary(cat.anova))
 
 # Regular lm: same computations as ANOVA but different way of showing results
 ## From the aov function help:
 ### The main difference from lm is in the way print, summary and so on handle the fit:
 ### this is expressed in the traditional language of the analysis of variance
 ### rather than that of linear models.
-stress.lm <- lm(StressLevel ~ Intervention*PrePost,
-                data = df)
-(summary(stress.lm))
+cat.lm <- lm(StressLevel ~ Intervention*PrePost,
+                data = cat)
+(summary(cat.lm))
 ## Result breakdown:
 ### Factor levels:
 #### Intervention = 0 for VideoGame, 1 for MindfulnessMeditation
@@ -112,19 +112,19 @@ stress.lm <- lm(StressLevel ~ Intervention*PrePost,
 ####                                                           diff. pre-int. when Meditation)
 
 # Mixed-effect model, just to compare output to ANOVA output and lm output
-stress.lmer <- lmer(StressLevel ~ Intervention*PrePost + (1 | Participant),
-                    data = df)
-(summary(stress.lmer))
+cat.lmer <- lmer(StressLevel ~ Intervention*PrePost + (1 | Participant),
+                    data = cat)
+(summary(cat.lmer))
 
 # Re-run aov and lm with different factor order for Intervention
-df2 <- df
-df2$Intervention <- relevel(df2$Intervention, ref = "Mindfulness Meditation") # New ref level
-stress.anova2 <- aov(StressLevel ~ Intervention*PrePost,
-                     data = df)
-(summary(stress.anova2)) # No difference
-stress.lm2 <- lm(StressLevel ~ Intervention*PrePost,
-                data = df2)
-(summary(stress.lm2))    # Now main effect of PrePost because this is for participants in the
-                         # Mindfulness Meditation group only
+cat2 <- cat
+cat2$Intervention <- relevel(cat2$Intervention, ref = "Mindfulness Meditation") # New ref level
+cat.anova2 <- aov(StressLevel ~ Intervention*PrePost,
+                     data = cat)
+(summary(cat.anova2)) # No difference
+cat.lm2 <- lm(StressLevel ~ Intervention*PrePost,
+                data = cat2)
+(summary(cat.lm2))    # Now main effect of PrePost because this is for participants in the
+                      # Mindfulness Meditation group only
 ## We can run the same result breakdown as before, but now we have different factor levels:
 ### Intervention = 0 for MindfulnessMeditation, 1 for VideoGame
